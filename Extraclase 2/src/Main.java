7d989064f97fe3dc21803063206fc5a0d448f503
DanielMontoyaR <danimo.rivera@gmail.com>
2020-10-21 23:05:09 -0600

    Se reintegra la carpeta con el código con los debidos ajustes

---
+++ Extraclase 2/src/Main.java
@@ -0,0 +1,25 @@
+import org.apache.log4j.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Daniel Montoya
+ */
+
+public class Main {
+	protected static final Logger log = Logger.getLogger(Excepciones.class);
+	
+	public static void main(String[] args) {
+		Excepciones eX = new Excepciones();
+        
+        eX.PruebaExcepciones();
+		
+	}
+}
+    
+
---
+++ README.txt
@@ -0,0 +1,3 @@
+Utilizando la tarea extraclase #1, cree un nuevo branch en GitHub donde incluya al menos cinco
+ejemplos de manejo de excepciones. Configure el framework de logging y asegúrese que todos los
+try - catch escriban las excepciones a un archivo.
